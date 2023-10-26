0c00534a3f0d7b0a1867cb2cbd7181b2b9a0c20d
Guilherme Tavares <joseserafimtavares89@gmail.com>
2023-10-26 03:30:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app.use(express.json());
 
 
 app.use(express.static(path.join(__dirname, 'build')));
+console.log(path.join(__dirname, 'build'))
 app.listen(4000, console.log('Aplicação esta online e pronta para rodar!'))
